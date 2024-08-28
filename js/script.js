4e25d1535957a4b562e2f5ea5b72700e8462c892
Marco-Guarnera <Darkaquon93@gmail.com>
2024-08-28 15:38:54 +0200

    Completed Data Structure

--- js/script.js
+++ js/script.js
@@ -1,8 +1,49 @@
-/*
-1.
-*/
-
 // Fase di preparazione
+
+// Strutture dati
+
+// Utilizzo un array di oggetti come struttura dati per i membri del team
+const team = [
+    {
+        fullName: "Wayne Barnett",
+        role: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        fullName: "Angela Caroll",
+        role: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg"
+    },
+
+    {
+        fullName: "Walter Gordon",
+        role: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+
+    {
+        fullName: "Angela Lopez",
+        role: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+        fullName: "Scott Estrada",
+        role: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+
+    {
+        fullName: "Barbara Ramos",
+        role: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+// Stampo la struttura dati in console
+console.table(team);
+
 // Fase di gestione eventi
 // Fase di raccolta dati
 // Fase di elaborazione
